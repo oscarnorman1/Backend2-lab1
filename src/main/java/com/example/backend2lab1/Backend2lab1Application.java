7f1140918e8f757f964a91510488c74fc28c9750
Oscar Norman <oscar.norman95@gmail.com>
2021-08-24 14:13:27 +0200

    letsgooo

---
+++ src/main/java/com/example/backend2lab1/Backend2lab1Application.java
@@ -0,0 +1,13 @@
+package com.example.backend2lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend2lab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend2lab1/Backend2lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend2lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2lab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
