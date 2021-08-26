e6e213d7146b484a21af71689cfc9fcf59ea6c59
Oscar Norman <oscar.norman95@gmail.com>
2021-08-26 08:53:17 +0200

    made styling into styles.css and added withdraw functionality

--- src/main/frontend/app.js
+++ src/main/frontend/app.js
@@ -11,16 +11,25 @@ depositBtn.addEventListener('click', () => {
     const name = document.querySelector('#name').value
     const amount = Number(document.querySelector('#amount').value)
 
-    fetchData(name, amount);
+    depositFetch(name, amount);
 });
 
 withdrawBtn.addEventListener('click', () => {
-    
+    const name = document.querySelector('#name').value
+    const amount = Number(document.querySelector('#amount').value)
+
+    withdrawFetch(name, amount);
 });
 
-const fetchData = (name, amount) => {
+const depositFetch = (name, amount) => {
 
     fetch(`http://localhost:8080/accounts/deposit/${name}/${amount}`)
         .then((data) => console.log(data))
         .catch((error) => console.log(error));
 };
+
+const withdrawFetch = (name, amount) => {
+    fetch(`http://localhost:8080/accounts/withdraw/${name}/${amount}`)
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+}
