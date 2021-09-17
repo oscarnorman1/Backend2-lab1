78dbb99c868a9628c2be5a99bae1da6ce0295952
baliharko <bali@fauxdelorean.com>
2021-09-17 09:39:58 +0200

    fixed ports in application.properties and docker-compose

--- src/test/java/com/example/backend2lab1/Backend2lab1ApplicationTests.java
+++ src/test/java/com/example/backend2lab1/Backend2lab1ApplicationTests.java
@@ -6,10 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Backend2lab1ApplicationTests {
 
-/*
-    @Test
-    void contextLoads() {
-    }
-*/
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
